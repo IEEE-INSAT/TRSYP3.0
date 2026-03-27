e19fb9ad6019a5a6195e15002736956955a4fc05
achourmohamedamine <mohamedamine.achour@insat.ucar.tn>
2026-03-27 19:43:37 +0100

    connecting to database

---
+++ backend/src/app.module.ts
@@ -0,0 +1,10 @@
+
+
+import { Global, Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+@Module({
+    imports:[PrismaModule],
+    providers:[AppService],
+})
+export class AppModule {}
