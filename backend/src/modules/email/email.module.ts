e25e6050e81049e0ba369c1101cb28cb1defb943
yassinekolsi <yassine.kolsi@gmail.com>
2026-07-14 13:54:41 +0100

    fix: wire SMTP transport into email service

--- backend/src/modules/email/email.module.ts
+++ backend/src/modules/email/email.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
-import { EmailService } from './service';
+import { createEmailTransport, EMAIL_TRANSPORT, EmailService } from './service';
 
 @Module({
-  providers: [EmailService],
+  providers: [
+    {
+      provide: EMAIL_TRANSPORT,
+      useFactory: createEmailTransport,
+    },
+    EmailService,
+  ],
   exports: [EmailService],
 })
 export class EmailModule {}
