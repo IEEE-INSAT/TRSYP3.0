459de1be7a7bd2940fc8280e58948013af4f86d6
MakkiAloulou <makkialoulou2005@gmail.com>
2026-09-15 21:45:06 +0100

    fix: remove unnecessary redirect

--- next.config.ts
+++ next.config.ts
@@ -13,21 +13,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: '(?:www\\.)?trsyp\\.ieee\\.tn',
-          },
-        ],
-        destination: 'https://rtc.ieee.tn/:path*',
-        permanent: true,
-      },
-    ];
-  },
   async headers() {
     return [
       {
